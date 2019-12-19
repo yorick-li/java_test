5dac43d68e6aa4a7b509d0db03ae458e57a87850
yorick-li <yorick_li2017@163.com>
2019-12-19 14:53:10 +0800

    MethodUtils--获取方法的执行结果<泛型>

--- java_base/reflection/src/main/java/com/yorick/pro/java_test/java_base/reflection/util/MethodUtils.java
+++ java_base/reflection/src/main/java/com/yorick/pro/java_test/java_base/reflection/util/MethodUtils.java
@@ -1,7 +1,7 @@
 package com.yorick.pro.java_test.java_base.reflection.util;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Objects;
 import java.util.Optional;
 
 /**
@@ -41,6 +41,54 @@ public class MethodUtils {
             Method method = optionalMethod.get();
             Class<?> returnType = method.getReturnType();
             return Optional.of(returnType);
+        }
+        return Optional.ofNullable(null);
+    }
+
+    /**
+     * 获取 obj 对象的、方法名为：methodName的方法的执行结果
+     * @param obj 调用方法的对象
+     * @param methodName 方法名称
+     * @param parameterType 方法参数类型
+     * @param parameters 实际调用时的方法参数
+     * @param returnType 返回结果类型
+     * @param <T>
+     * @param <U>
+     * @return
+     */
+    public <T, U> U getInvokeObj(T obj, String methodName, Class<?>[] parameterType, Object[] parameters, Class<U> returnType) {
+        Optional<Object> optionalInvokeObj = getInvokeObj(obj, methodName, parameterType, parameters);
+        if (optionalInvokeObj.isPresent()) {
+            Object invokeObj = optionalInvokeObj.get();
+            return returnType.cast(invokeObj);
+        }
+        return null;
+    }
+
+    /**
+     * 获取 obj 对象的、方法名为：methodName的方法的执行结果
+     * @param obj 调用方法的对象
+     * @param methodName 方法名称
+     * @param parameterType 方法参数类型
+     * @param parameters 实际调用时的方法参数
+     * @param <T>
+     * @return
+     */
+    public <T> Optional<Object> getInvokeObj(T obj, String methodName, Class<?>[] parameterType, Object[] parameters) {
+        Optional<Method> optionalMethod = getMethod(obj, methodName, parameterType);
+        if (optionalMethod.isPresent()) {
+            Method method = optionalMethod.get();
+            method.setAccessible(Boolean.TRUE);
+            try {
+                Object methodInvokeResult = method.invoke(obj, parameters);
+                return Optional.of(methodInvokeResult);
+            } catch (IllegalAccessException e) {
+                System.out.println("类：" + obj.getClass().getName() + ", 无：" + methodName + "方法的访问权限！");
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                System.out.println("类：" + obj.getClass().getName() + ", " + methodName + "方法调用异常！");
+                e.printStackTrace();
+            }
         }
         return Optional.ofNullable(null);
     }
